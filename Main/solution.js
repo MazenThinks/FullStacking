4b7f21cef3efd586d1f19e2740386bcfa19c09ba
Mazen Magdy <mazenmagdy190.mm@gmail.com>
2025-02-28 01:48:40 +0200

    NPM / readfile

--- Main/solution.js
+++ Main/solution.js
@@ -1,10 +1,9 @@
 const fs = require("fs");
 
-// fs.readFile("message.txt", "Fuck You", (err) => {
-//   if (err) {
-//     console.error(err);
-//   }
-// });
+fs.writeFile("message.txt", "Hello Node", (err) => {
+  if (err) throw err;
+  console.log("The file has been saved!");
+});
 
 fs.readFile("message.txt", "utf8", (err, data) => {
   if (err) throw err;
