30bc15137034849c3043329c7ad9b08617a91642
Mazen Magdy <mazenmagdy190.mm@gmail.com>
2025-03-20 23:29:33 +0200

    EJS / Making first express.js project

--- Main/index.js
+++ Main/index.js
@@ -1,27 +1,7 @@
-/* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
+import express from "express";
+const app = express();
+const port = 3000;
 
-import inquirer from "inquirer";
-import qr from "qr-image";
-import fs from "fs";
-
-inquirer
-  .prompt([
-    {
-      name: "url",
-      message: "Enter your url:",
-    },
-  ])
-  .then((answers) => {
-    console.log("Submitted!");
-    fs.writeFile("urltext", answers.url, (err) => {
-      if (err) {
-        console.error(err);
-      }
-    });
-    var qr_svg = qr.image(answers.url, { type: "png" });
-    qr_svg.pipe(fs.createWriteStream("urlimg.png"));
-  });
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
