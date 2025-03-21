82a48c08eac1b82b919121d2b918aabc4a98555f
Mazen Magdy <mazenmagdy190.mm@gmail.com>
2025-03-22 01:41:46 +0200

    EJS / Custom Middleware

--- Main/index3.js
+++ Main/index3.js
@@ -3,6 +3,12 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+const logger = (req, res, next) => {
+  console.log("request method: ", req.method);
+  console.log("request path: ", req.path);
+  next();
+};
+
 app.use(logger);
 
 app.get("/", (req, res) => {
