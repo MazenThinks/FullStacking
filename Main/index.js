4a874daf2a31d72b138c7919b9df5ad6478cd530
Mazen <mazenmagdy190.mm@gmail.com>
2025-04-13 11:01:37 +0200

    EJS / Secrets Access Project

---
+++ Main/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var userIsAuthorised = false;
+
+function passcheck(req, res, next) {
+  const pass = "Yoo";
+  var userpass = req.body["password"];
+  if (userpass === pass) {
+    userIsAuthorised = true;
+  }
+  next();
+}
+app.use(passcheck);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  if (userIsAuthorised == true) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.redirect("/");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
