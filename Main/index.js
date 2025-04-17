3e2718d57e8601be6c74355b797811da1383d6f2
Mazen <mazenmagdy190.mm@gmail.com>
2025-04-17 11:55:11 +0200

    EJS / Tags : list appears in even seconds only ..

--- Main/index.js
+++ Main/index.js
@@ -1,43 +1,17 @@
 import express from "express";
-import bodyParser from "body-parser";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
 const app = express();
 const port = 3000;
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.get("/", (req, res) => {
-  const d = new Date();
-  const day = d.getDay();
-  const cday = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-
-  let dayy = "";
-  let advicee = "";
-
-  if (day >= 5) {
-    (dayy = cday[day]), (advicee = "its time to work hard");
-  } else {
-    (dayy = cday[day]), (advicee = "its time to relax");
-  }
-
-  res.render("index.ejs", {
-    dayType: dayy,
-    advice: advicee,
-  });
+  const data = {
+    title: "EJS Tags",
+    seconds: new Date().getSeconds(),
+    items: ["apple", "banana", "cherry"],
+    htmlContent: "<strong>This is some strong text</strong>",
+  };
+  res.render("index.ejs", data);
 });
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
