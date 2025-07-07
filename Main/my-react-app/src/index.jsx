8b1cc06616787e3038def9cceefd700b707b3e91
Mazen <mazenmagdy190.mm@gmail.com>
2025-07-07 15:42:19 +0300

    ReactJS / Inline Styling, Attributes

--- Main/my-react-app/src/index.jsx
+++ Main/my-react-app/src/index.jsx
@@ -1,14 +1,28 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/";
+
+const color = {
+  color: "black",
+};
+
+if (new Date().getHours() < 12) {
+  var Greeting = "Good Morning!";
+  color.color = "red";
+} else if (new Date().getHours() < 18) {
+  var Greeting = "Good Afternoon!";
+  color.color = "green";
+} else {
+  var Greeting = "Good Evening!";
+  color.color = "blue";
+}
+
+const date = new Date();
+console.log(`Current date and time:`);
 
 ReactDOM.render(
   <>
-    <h1>Hello World!</h1>
-    <ul>
-      <li>Okay</li>
-      <li>Okayy</li>
-      <li>Okayyy</li>
-    </ul>
+    <h1 style={color}>{Greeting}</h1>
+    <div></div>
   </>,
   document.getElementById("root")
 );
