8add0bfcea86201a6b22caa60e476ccc07a4cb81
Mazen <mazenmagdy190.mm@gmail.com>
2025-07-07 13:52:09 +0300

    ReactJS / Hello World.

---
+++ Main/my-react-app/src/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <>
+    <h1>Hello World!</h1>
+    <ul>
+      <li>Okay</li>
+      <li>Okayy</li>
+      <li>Okayyy</li>
+    </ul>
+  </>,
+  document.getElementById("root")
+);
